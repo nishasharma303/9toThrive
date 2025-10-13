fd90cbe1b3e7ad28a20da539b334a437a72bf5d1
HrishitaRaj <hrishitaraj@gmail.com>
2025-10-13 14:12:43 +0530

    resume-uploader

--- src/contexts/ResumeContext.tsx
+++ src/contexts/ResumeContext.tsx
@@ -17,7 +17,7 @@ export const ResumeProvider: React.FC<React.PropsWithChildren<{}>> = ({ children
 
 export function useResume() {
   const ctx = useContext(ResumeContext);
-  if (!ctx) throw new Error("useResume must be used within a ResumeProvider");
+  if (!ctx) throw new Error("useResume must be used within the ResumeProvider");
   return ctx;
 }
 
